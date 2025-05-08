f6d1bd8fc94b87037df4989764a8be874687923a
Artem <darters@mail.ru>
2025-05-08 16:51:37 +0300

    ports changed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': { target: 'http://localhost:3001', changeOrigin: true }
+      '/api': { target: 'http://localhost:3003', changeOrigin: true }
     }
   }
 });
